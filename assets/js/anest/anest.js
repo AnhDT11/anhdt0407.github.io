3541bce5d0af70212dffd34e5964a0b5424058cb
AnhDT <s2.25111211@gmail.com>
2018-12-29 22:34:07 +0700

    add map

--- assets/js/anest/anest.js
+++ assets/js/anest/anest.js
@@ -30,4 +30,20 @@ $(document).ready(function() {
         $(this).toggleClass('rotate');
     });
 
+    // Smooth Scrolling
+    $(".nav-bar a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+
 });
